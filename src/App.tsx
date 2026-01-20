30094887e401d42e722088107a9a5dbf0bba54aa
Leonkazaryan <leon.kazaryan@nu.edu.kz>
2026-01-20 12:53:02 +0500

    Inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,37 @@
-import App from './components/App'
+import { useEffect } from 'react'
+import { useTypingStore } from './store/useTypingStore'
+import TypingTest from './components/TypingTest'
+import Settings from './components/Settings'
+import Stats from './components/Stats'
+import Header from './components/Header'
+import Keyboard from './components/Keyboard'
+import Confetti from './components/Confetti'
+import BackgroundEffects from './components/BackgroundEffects'
+import { getThemeClasses } from './utils/themes'
+
+function App() {
+  const theme = useTypingStore((state) => state.settings.theme)
+  const themeClasses = getThemeClasses(theme)
+  
+  useEffect(() => {
+    document.body.className = themeClasses.body
+  }, [themeClasses])
+  
+  return (
+    <div className={`min-h-screen transition-colors duration-500 ${themeClasses.bg} relative`}>
+      <BackgroundEffects />
+      <Confetti />
+      <div className="container mx-auto px-4 py-8 max-w-6xl relative z-10">
+        <Header />
+        <div className="mt-8 space-y-6">
+          <Settings />
+          <TypingTest />
+          <Keyboard />
+          <Stats />
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default App
