d8846d52cef6ee409ddd991b5d0515509a717c80
Leonkazaryan <leon.kazaryan@nu.edu.kz>
2026-01-28 18:50:30 +0500

    Deploy Import Fix

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-import { prisma } from './db/prisma'
+import { prisma } from './db/prisma.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
